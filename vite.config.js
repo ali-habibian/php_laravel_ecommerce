b49c9e1874226d5de0f69b21d8a9b05ee23415c6
Ali Habibian <ali.habibian04@gmail.com>
2024-12-01 16:12:31 +0330

    Added js files for admin template

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/scss/admin/admin.scss'],
+            input: ['resources/css/app.css', 'resources/js/admin/admin.js', 'resources/scss/admin/admin.scss'],
             refresh: true,
         }),
     ],
