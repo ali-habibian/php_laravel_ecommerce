48d34676fdb42e6b5047b3b6e0092b1e831e0a71
Ali Habibian <ali.habibian04@gmail.com>
2024-12-03 20:22:27 +0330

    feat: added attributes rout to sidebar

--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -33,6 +33,20 @@
             <span> برندها </span></a>
     </li>
 
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProducts" aria-expanded="true"
+           aria-controls="collapseProducts">
+            <i class="fas fa-fw fa-cart-plus"></i>
+            <span> محصولات </span>
+        </a>
+        <div id="collapseProducts" class="collapse" aria-labelledby="headingProducts" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('admin.attributes.index') }}">ویژگی ها</a>
+            </div>
+        </div>
+    </li>
+
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
