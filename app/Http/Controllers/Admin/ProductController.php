4d22da0154c2c4be487d22a424543cb45b2194f5
Ali Habibian <ali.habibian04@gmail.com>
2024-12-09 20:36:36 +0330

    feat: implemented creating and storing new product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,11 +5,27 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductAttribute;
+use App\Models\ProductImage;
+use App\Models\ProductVariation;
 use App\Models\Tag;
+use App\Services\UploadService;
+use DB;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
+    protected UploadService $uploadService;
+
+    public function __construct(UploadService $uploadService)
+    {
+        // Inject the UploadService into the controller
+        $this->uploadService = $uploadService;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -47,7 +63,7 @@ class ProductController extends Controller
             'images.*' => 'required|image|mimes:jpeg,png,jpg,svg|max:2048',
             'category_id' => 'required|integer|exists:categories,id',
             'attribute_ids' => 'required|array',
-            'attribute_ids.*' => 'required|integer|exists:attributes,id',
+            'attribute_ids.*' => 'required|string',
             'variation_values' => 'required|array',
             'variation_values.*.*' => 'required',
             'variation_values.value.*' => 'string',
@@ -57,6 +73,66 @@ class ProductController extends Controller
             'delivery_amount' => 'nullable|integer|min:0',
             'delivery_amount_per_product' => 'nullable|integer|min:0'
         ]);
+
+        try {
+            DB::beginTransaction();
+
+            $primaryImageFile = $request->file('primary_image');
+            $imageFiles = $request->file('images');
+
+            $primaryImageUploadedPath = $this->uploadService->uploadFile($primaryImageFile, config('uploads.product_images_path'), 'img_');
+            $imagesUploadedPaths = $this->uploadService->uploadFiles($imageFiles, config('uploads.product_images_path'), 'img_');
+
+            $product = Product::create([
+                'name' => $request->name,
+                'brand_id' => $request->brand_id,
+                'category_id' => $request->category_id,
+                'primary_image' => $primaryImageUploadedPath,
+                'description' => $request->description,
+                'is_active' => $request->is_active,
+                'delivery_amount' => $request->delivery_amount,
+                'delivery_amount_per_product' => $request->delivery_amount_per_product
+            ]);
+
+            foreach ($imagesUploadedPaths as $imageUploadedPath) {
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => $imageUploadedPath
+                ]);
+            }
+
+            foreach ($request->attribute_ids as $key => $value) {
+                ProductAttribute::create([
+                    'product_id' => $product->id,
+                    'attribute_id' => $key,
+                    'value' => $value,
+                ]);
+            }
+
+            $counter = count($request->variation_values['value']);
+            $category = Category::findOrFail($request->category_id);
+            $variationAttributeId = $category->attributeList()->where('is_variation', true)->first()->id;
+
+            for ($i = 0; $i < $counter; $i++) {
+                ProductVariation::create([
+                    'product_id' => $product->id,
+                    'attribute_id' => $variationAttributeId,
+                    'value' => $request->variation_values['value'][$i],
+                    'price' => $request->variation_values['price'][$i],
+                    'quantity' => $request->variation_values['quantity'][$i],
+                    'sku' => $request->variation_values['sku'][$i]
+                ]);
+            }
+
+            $product->tags()->sync($request->tag_ids);
+
+            DB::commit();
+        } catch (Exception $e) {
+            Log::error($e);
+            return redirect()->back()->with('error', 'مشکلی در ایجاد محصول رخ داده است');
+        }
+
+        return redirect()->route('admin.products.index')->with('success', 'محصول با موفقیت ایجاد شد');
     }
 
     /**
@@ -64,7 +140,10 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        echo $product->brand->name . '<br>';
+        echo $product->category->name . '<br>';
+        echo $product->productImages;
     }
 
     /**
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
+    use Sluggable;
+
     protected $table = 'products';
     protected $guarded = [];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function productImages(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function productAttributes(): HasMany
+    {
+        return $this->hasMany(ProductAttribute::class);
+    }
+
+    public function productVariations(): HasMany
+    {
+        return $this->hasMany(ProductVariation::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'product_tag');
+    }
 }
