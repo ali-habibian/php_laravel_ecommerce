12fe06b3cbc081da472037c5fd7f3e1712df2993
Ali Habibian <ali.habibian04@gmail.com>
2024-12-16 20:56:31 +0330

    feat: implemented showing slider type banners

---
+++ app/Constants/BannerTypes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Constants;
+
+/**
+ * Class to define banner types
+ *
+ * This class contains constants representing different types of banners used in the application.
+ */
+class BannerTypes
+{
+    public const SLIDER = 'slider';
+    public const INDEX_BOTTOM = 'index_bottom';
+    public const INDEX_TOP = 'index_top';
+}
+
--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Constants\BannerTypes;
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $sliders = Banner::where('type', BannerTypes::SLIDER)
+            ->where('is_active', true)
+            ->orderBy('priority')
+            ->get();
+
+        return view('home.index', compact('sliders'));
     }
 }
