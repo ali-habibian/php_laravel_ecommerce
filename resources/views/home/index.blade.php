f52dca833d773438b611baed2c3bd80200bdbdb4
Ali Habibian <ali.habibian04@gmail.com>
2025-01-15 22:44:15 +0330

    feat: added some images to index page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -129,12 +129,12 @@
                                                             <span class="ht-product-action-tooltip"> مقایسه </span>
                                                         </a>
                                                     </li>
-                                                    <li>
-                                                        <a href="#">
-                                                            <i class="sli sli-bag"></i>
-                                                            <span class="ht-product-action-tooltip"> افزودن به سبد </span>
-                                                        </a>
-                                                    </li>
+{{--                                                    <li> because we have variations and user should select it first then add to basket --}}
+{{--                                                        <a href="#">--}}
+{{--                                                            <i class="sli sli-bag"></i>--}}
+{{--                                                            <span class="ht-product-action-tooltip"> افزودن به سبد </span>--}}
+{{--                                                        </a>--}}
+{{--                                                    </li>--}}
                                                 </ul>
                                             </div>
                                         </div>
@@ -192,14 +192,14 @@
     </div>
     {{-- End Product Area 1 --}}
 
-    {{-- Testimonial Area --}}
-    <div class="testimonial-area pt-80 pb-95 section-margin-1" style="background-image: url(assets/img/bg/bg-1.jpg);">
+    {{-- Testimonial Area (Users Comments) --}}
+    <div class="testimonial-area pt-80 pb-95 section-margin-1" style="background-image: url({{ asset('images/home/bg/bg-1.jpg') }});">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 ml-auto mr-auto">
                     <div class="testimonial-active owl-carousel nav-style-1">
                         <div class="single-testimonial text-center">
-                            <img src="assets/img/testimonial/testi-1.png" alt=""/>
+                            <img src="{{ asset('images/home/testimonial/testi-1.png') }}" alt=""/>
                             <p>
                                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک
                                 است. چاپگرها و
@@ -210,12 +210,12 @@
                                 آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت
                             </p>
                             <div class="client-info">
-                                <img src="assets/img/icon-img/testi.png" alt=""/>
+                                <img src="{{ asset('images/home/icon-img/testi.png') }}" alt=""/>
                                 <h5>لورم ایپسوم</h5>
                             </div>
                         </div>
                         <div class="single-testimonial text-center">
-                            <img src="assets/img/testimonial/testi-2.png" alt=""/>
+                            <img src="{{ asset('images/home/testimonial/testi-2.png') }}" alt=""/>
                             <p>
                                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک
                                 است. چاپگرها و
@@ -226,7 +226,7 @@
                                 آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت
                             </p>
                             <div class="client-info">
-                                <img src="assets/img/icon-img/testi.png" alt=""/>
+                                <img src="{{ asset('images/home/icon-img/testi.png') }}" alt=""/>
                                 <h5>لورم ایپسوم</h5>
                             </div>
                         </div>
@@ -238,7 +238,7 @@
     {{-- End Testimonial Area --}}
 
     {{-- Product Area 2 --}}
-    <div class="product-area pt-95 pb-70">
+    {{--<div class="product-area pt-95 pb-70">
         <div class="container">
             <div class="section-title text-center pb-60">
                 <h2>لورم ایپسوم</h2>
@@ -604,11 +604,11 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
     {{-- End Product Area 2 --}}
 
     {{-- Index Bottom Banner Area --}}
-    <div class="banner-area pb-120">
+    <div class="banner-area pt-80 pb-120">
         <div class="container">
             <div class="row">
                 @foreach($indexBottomBanners as $banner)
@@ -635,7 +635,7 @@
                 <div class="col-xl-4 col-lg-4 col-md-4">
                     <div class="single-feature text-right mb-40">
                         <div class="feature-icon">
-                            <img src="assets/img/icon-img/free-shipping.png" alt=""/>
+                            <img src="{{ asset("images/home/icon-img/free-shipping.png") }}" alt=""/>
                         </div>
                         <div class="feature-content">
                             <h4>لورم ایپسوم</h4>
@@ -646,7 +646,7 @@
                 <div class="col-xl-4 col-lg-4 col-md-4">
                     <div class="single-feature text-right mb-40 pl-50">
                         <div class="feature-icon">
-                            <img src="assets/img/icon-img/support.png" alt=""/>
+                            <img src="{{ asset("images/home/icon-img/support.png") }}" alt=""/>
                         </div>
                         <div class="feature-content">
                             <h4>لورم ایپسوم</h4>
@@ -657,7 +657,7 @@
                 <div class="col-xl-4 col-lg-4 col-md-4">
                     <div class="single-feature text-right mb-40">
                         <div class="feature-icon">
-                            <img src="assets/img/icon-img/security.png" alt=""/>
+                            <img src="{{ asset("images/home/icon-img/security.png") }}" alt=""/>
                         </div>
                         <div class="feature-content">
                             <h4>لورم ایپسوم</h4>
