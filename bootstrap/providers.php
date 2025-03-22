e4cf0305f5120d8673a08fc3739aa4f194e8c73f
Ali Habibian <ali.habibian04@gmail.com>
2025-03-22 20:12:31 +0330

    build(deps): add SEOTools package for Laravel
    - Added Artesaos\SEOTools\Providers\SEOToolsServiceProvider to providers.php
    - Added seotools config file to config directory- Updated composer.json to include artesaos/seotools package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    Darryldecode\Cart\CartServiceProvider::class
+    Darryldecode\Cart\CartServiceProvider::class,
+    Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class
 ];
