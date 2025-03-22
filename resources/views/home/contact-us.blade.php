e099cbb3683106eb03aa507e86b4ace0a1aee195
Ali Habibian <ali.habibian04@gmail.com>
2025-03-22 19:00:53 +0330

    feat: implemented contact-us page's map

--- resources/views/home/contact-us.blade.php
+++ resources/views/home/contact-us.blade.php
@@ -99,3 +99,27 @@
         </div>
     </div>
 @endsection
+
+@push('style')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossorigin=""/>
+@endpush
+
+@push('scripts')
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
+
+    <script>
+        let map = L.map('map').setView([{{ $setting->latitude }}, {{ $setting->longitude }}], 15);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        let marker = L.marker([{{ $setting->latitude }}, {{ $setting->longitude }}]).addTo(map);
+        marker.bindPopup("<b>ما اینجاییم</b>").openPopup();
+    </script>
+@endpush
