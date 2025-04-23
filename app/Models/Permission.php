f6817133593d4d01e9161526ea575353e41089d8
Ali Habibian <ali.habibian04@gmail.com>
2025-04-23 14:31:54 +0330

    feat(auth): Add Spatie's Laravel Permission package as a dependency
    - Create Permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class Permission extends Model
+{
+    use HasRoles;
+    
+    protected $table = 'permissions';
+    protected $guarded = [];
+}
